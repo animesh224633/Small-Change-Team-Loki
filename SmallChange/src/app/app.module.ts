9d1d01fe6dd5bc9cada7c8a58f6a5c432bde86f6
Seneha <senehababu2001@gmail.com>
2022-09-05 11:52:08 +0530

    Structure

--- SmallChange/src/app/app.module.ts
+++ SmallChange/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavBarComponent } from './nav-bar/nav-bar/nav-bar.component';
+import { NavLinkComponent } from './Shared/nav-link/nav-link.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    NavBarComponent,
+    NavLinkComponent
   ],
   imports: [
     BrowserModule,
