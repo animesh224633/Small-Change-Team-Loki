21f9d88e391af9d8f0585f95b18360e58eddb4c2
Seneha <senehababu2001@gmail.com>
2022-09-05 15:40:22 +0530

    Nav bar Component

--- SmallChange/src/app/app-routing.module.ts
+++ SmallChange/src/app/app-routing.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { ActivityComponent } from './routing-nav-bar/activity/activity.component';
+import { PortfolioComponent } from './routing-nav-bar/portfolio/portfolio.component';
+import { PreferencesComponent } from './routing-nav-bar/preferences/preferences.component';
+import { TradeComponent } from './routing-nav-bar/trade/trade.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginPageComponent }
+  { path: 'login', component: LoginPageComponent }, 
+  { path: 'portfolio', component: PortfolioComponent},
+  { path: 'activity', component: ActivityComponent },
+  { path: 'trade', component: TradeComponent },
+  { path: 'preferences', component: PreferencesComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  
 })
 export class AppRoutingModule { }
--- SmallChange/src/app/app.module.ts
+++ SmallChange/src/app/app.module.ts
@@ -7,13 +7,21 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavBarComponent } from './nav-bar/nav-bar/nav-bar.component';
 import { NavLinkComponent } from './Shared/nav-link/nav-link.component';
+import { PortfolioComponent } from './routing-nav-bar/portfolio/portfolio.component';
+import { ActivityComponent } from './routing-nav-bar/activity/activity.component';
+import { TradeComponent } from './routing-nav-bar/trade/trade.component';
+import { PreferencesComponent } from './routing-nav-bar/preferences/preferences.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
     NavBarComponent,
-    NavLinkComponent
+    NavLinkComponent,
+    PortfolioComponent,
+    ActivityComponent,
+    TradeComponent,
+    PreferencesComponent
   ],
   imports: [
     BrowserModule,
