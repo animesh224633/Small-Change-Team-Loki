e68a424bf494efd21c1ab77ec62e0bfc02adbfc2
Danish Kidwai, Muhammad <muhammad.danishkidwai@fmr.com>
2022-09-06 17:00:03 +0530

    Tried material

--- SmallChange/src/app/app.module.ts
+++ SmallChange/src/app/app.module.ts
@@ -6,6 +6,12 @@ import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatFormFieldModule, MatLabel, MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -16,9 +22,16 @@ import { LoginFormComponent } from './login-form/login-form.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
-  providers: [],
+  providers: [{provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'fill'}}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- SmallChange/src/app/login-form/login-form.component.ts
+++ SmallChange/src/app/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginFormComponent implements OnInit {
 
-  constructor() { }
+  public loginValid = true;
+  public username = '';
+  public password = '';
 
+  loginForm!: FormGroup;
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      userName: new FormControl('', [Validators.required, Validators.minLength(4),Validators.maxLength(20)]),
+      password: new FormControl('', [Validators.required, Validators.pattern("^[0-9a-zA-Z]*$"),Validators.minLength(6), Validators.maxLength(24)])
+    })
+  }
+  get userNameField(): any {
+    return this.loginForm.get('userName');
+  }
+  get passwordField(): any {
+    return this.loginForm.get('password');
+  }
+  loginFormSubmit(): void {
+    console.log(this.loginForm.value);
+    // Call Api
+  }
+
+  public onSubmit(): void {
+    this.loginValid = true;
   }
 
 }
