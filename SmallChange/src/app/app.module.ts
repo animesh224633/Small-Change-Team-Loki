aee361112c128613d150d323f1c64ecd9c4c68ea
Animesh <animesh.aj.1999@gmail.com>
2022-09-13 16:39:06 +0530

    sxaaa

--- SmallChange/src/app/app.module.ts
+++ SmallChange/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -69,7 +70,8 @@ import { PortfolioDetailsComponent } from './portfolio/portfolio-details/portfol
     HttpClientModule,
     MatDividerModule,
     MatIconModule,
-    MatTableModule
+    MatTableModule,
+    MatPaginatorModule
   ],
   providers: [{provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'fill'}}],
   bootstrap: [AppComponent]
