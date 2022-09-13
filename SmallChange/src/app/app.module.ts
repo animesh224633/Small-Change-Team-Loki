2577035c83bcc7aec6815163d0ee475b373b97c9
Danish Kidwai, Muhammad <muhammad.danishkidwai@fmr.com>
2022-09-13 13:09:06 +0530

    added framework for portfolio component

--- SmallChange/src/app/app.module.ts
+++ SmallChange/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { LoginFormComponent } from './login-form/login-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
 import {MatInputModule} from '@angular/material/input'
 import {MatFormFieldModule, MatLabel, MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -27,6 +28,9 @@ import {MatTabsModule} from '@angular/material/tabs';
 import { HttpClientModule } from '@angular/common/http';
 import {MatDividerModule} from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { PortfolioStockTableComponent } from './portfolio/portfolio-stock-table/portfolio-stock-table.component';
+import { PortfolioMfTableComponent } from './portfolio/portfolio-mf-table/portfolio-mf-table.component';
+import { PortfolioDetailsComponent } from './portfolio/portfolio-details/portfolio-details.component';
 
 
 @NgModule({
@@ -43,7 +47,10 @@ import { MatIconModule } from '@angular/material/icon';
     PortfolioComponent,
     ActivityComponent,
     TradeComponent,
-    PreferencesComponent
+    PreferencesComponent,
+    PortfolioStockTableComponent,
+    PortfolioMfTableComponent,
+    PortfolioDetailsComponent
   ],
   imports: [
     BrowserModule,
@@ -61,7 +68,8 @@ import { MatIconModule } from '@angular/material/icon';
     MatTabsModule,
     HttpClientModule,
     MatDividerModule,
-    MatIconModule
+    MatIconModule,
+    MatTableModule
   ],
   providers: [{provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'fill'}}],
   bootstrap: [AppComponent]
---
+++ SmallChange/src/app/portfolio/portfolio-stock-table/portfolio-stock-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-stock-table',
+  templateUrl: './portfolio-stock-table.component.html',
+  styleUrls: ['./portfolio-stock-table.component.css']
+})
+export class PortfolioStockTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
