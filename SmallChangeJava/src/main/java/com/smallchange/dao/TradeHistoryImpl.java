4aece8fbc79e7cab488c7b3a7ad0e8b638a232c0
animesh224633 <animesh.aj.1999@gmail.com>
2022-10-12 12:34:06 +0530

    Added jdbc

---
+++ SmallChangeJava/src/main/java/com/smallchange/dao/TradeHistoryImpl.java
@@ -0,0 +1,49 @@
+package com.smallchange.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+
+
+
+public class TradeHistoryImpl implements TradeHistory {
+	
+	
+	private DataSource datasource;
+
+	public TradeHistoryImpl(DataSource datasource) {
+		this.datasource = datasource;
+	}
+
+	@Override
+	public List<TradeHistory> getTradeHistory(String clientId) {
+		String sql="""
+				select client_id from client where client_id='1'
+				""";
+		try (Connection conn = datasource.getConnection(); 
+				PreparedStatement stmt = conn.prepareStatement(sql)){
+			System.out.println("hi");
+			ResultSet rs=stmt.executeQuery();
+			System.out.println("hhh");
+			List<TradeHistory> client=new ArrayList<>();
+			int count=0;
+			while(rs.next()) {
+				count++;
+			}
+			System.out.println(count);
+			return null;
+			
+		}catch(SQLException e){
+			throw new IllegalArgumentException("Database Access Error");
+			
+		}
+	}
+}
+
+
---
+++ SmallChangeJava/src/main/java/com/smallchange/uimodel/SellInstrument.java
@@ -0,0 +1,58 @@
+package com.smallchange.uimodel;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class SellInstrument {
+	private String code;
+
+	private String name;
+	private BigDecimal currentPrice;
+	private String category;
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public BigDecimal getCurrentPrice() {
+		return currentPrice;
+	}
+	public void setCurrentPrice(BigDecimal currentPrice) {
+		this.currentPrice = currentPrice;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(category, code, currentPrice, name);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SellInstrument other = (SellInstrument) obj;
+		return Objects.equals(category, other.category) && Objects.equals(code, other.code)
+				&& Objects.equals(currentPrice, other.currentPrice) && Objects.equals(name, other.name);
+	}
+	@Override
+	public String toString() {
+		return "SellInstrument [code=" + code + ", name=" + name + ", currentPrice=" + currentPrice + ", category="
+				+ category + "]";
+	}
+
+}
