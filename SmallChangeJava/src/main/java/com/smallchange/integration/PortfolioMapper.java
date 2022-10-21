40131ef6c885ae0ad4aa9a4cd0680b428014f95a
animesh224633 <animesh.aj.1999@gmail.com>
2022-10-21 14:52:13 +0530

    portffd

--- SmallChangeJava/src/main/java/com/smallchange/integration/PortfolioMapper.java
+++ SmallChangeJava/src/main/java/com/smallchange/integration/PortfolioMapper.java
@@ -2,10 +2,15 @@ package com.smallchange.integration;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.smallchange.uimodel.Portfolio;
 
 
 public interface PortfolioMapper {
-	List<Portfolio> getUserPortfolio(String clientId);
+	List<Portfolio> getUserPortfolio(
+			@Param("clientId") String clientId
+			);
+	
 
 }
--- SmallChangeJava/src/test/java/com/smallchange/integration/PortfolioDaoMyBatisImplTest.java
+++ SmallChangeJava/src/test/java/com/smallchange/integration/PortfolioDaoMyBatisImplTest.java
@@ -2,6 +2,7 @@ package com.smallchange.integration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
@@ -15,7 +16,9 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.smallchange.dao.DatabaseException;
 import com.smallchange.uimodel.Portfolio;
+import com.smallchange.uimodel.PortfolioStock;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration("classpath:beans.xml")
@@ -27,6 +30,9 @@ class PortfolioDaoMyBatisImplTest {
 	
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
+	
+
+	
 
 	
 
@@ -36,5 +42,33 @@ class PortfolioDaoMyBatisImplTest {
 		List<Portfolio> clientPortfolio = dao.getUserPortfolio("1");
 		assertEquals(2,clientPortfolio.size());
 	}
+	
+	
+	
+	
+	
+	@DisplayName("Should return 0 when client id is zero")
+	@Test
+	void getClientWithClientIdZero() {
+		assertThrows(NullPointerException.class, () -> {
+			dao.getUserPortfolio(null);
+		});
+	}
+	@DisplayName("Should contain amazon when retrieving client 1's portfolio")
+	@Test
+	void client1ShouldConatinAmazon() {
+		Portfolio client1=null;
+
+		PortfolioStock amazon = new PortfolioStock( "AMAZON",  "AMZN", 1 , BigDecimal.valueOf(5694.70).setScale(2), BigDecimal.valueOf(100.80).setScale(2),
+				BigDecimal.valueOf(5694.70).setScale(2), BigDecimal.valueOf(100.80).setScale(2),  BigDecimal.valueOf(-5593.90), BigDecimal.valueOf(-98.00).setScale(2));
+		 client1=new Portfolio();
+		client1.setPortfolioStockView(amazon);
+		List<Portfolio> clientPortfolio = dao.getUserPortfolio("1");
+		System.out.println(clientPortfolio.get(0).getPortfolioStockView().getCode());
+		assertEquals(client1.getPortfolioStockView().getCode(),clientPortfolio.get(0).getPortfolioStockView().getCode());
+		
+		//assertTrue(clientPortfolio.contains(client1));
+	}
+	
 
 }
